27413a600c18ce116cc83daa1bfc6c08c6dff8b2
CloudyNC <geoffreykellync@gmail.com>
2022-02-10 20:35:23 -0500

    started pokecard function

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 import 'regenerator-runtime/runtime';
 import axios from 'axios';
-
+console.log("It's working!");
 
 
 
@@ -14,4 +14,35 @@ function getPokeInfo(pokeName){
 
 }
 
+getPokeInfo('squirtle');
 
+
+{/* 
+    <div class="card">
+      <img src={image url of user} />
+      <div class="card-info">
+        <h3 class="name">{users name}</h3>
+        <p class="username">{users user name}</p>
+        <p>Location: {users location}</p>
+        <p>Profile:
+          <a href={address to users github page}>{address to users github page}</a>
+        </p>
+        <p>Followers: {users followers count}</p>
+        <p>Following: {users following count}</p>
+        <p>Bio: {users bio}</p>
+      </div>
+    </div>
+
+*/}
+
+
+
+const pokeCard = pokecard => {
+  const card = document.createElement("div");
+  const pokeImg = document.createElement("img");
+  
+
+
+
+
+}
