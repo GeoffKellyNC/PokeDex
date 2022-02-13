869cce8b3ef12b933207c2bab34b7c6320e26959
CloudyNC <geoffreykellync@gmail.com>
2022-02-12 22:59:15 -0500

    added abilities card

--- scripts/app.js
+++ scripts/app.js
@@ -3,46 +3,83 @@ import axios from 'axios';
 console.log("It's working!");
 
 
+const pokemonName = 'pikachu';
 
-function getPokeInfo(pokeName){
-  axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
-    .then(response => {
-  console.log(response.data);
+const pokeCardInfo = ({
+  name,
+  height,
+  weight
+}) => {
+  const infoCard = document.createElement("div");
+  const infoHeading = document.createElement("h2")
+  const pokeImg = document.createElement("img");
+  const pokeName = document.createElement("h3")
+  const pokeHeight = document.createElement("p")
+  const pokeWeight = document.createElement("p")
+
+  infoCard.classList.add("card-container")
+  pokeImg.classList.add("poke-img");
+  pokeImg.src = "https://www.freepnglogos.com/uploads/pokemon-symbol-logo-png-31.png"
+  pokeName.classList.add("poke-name", "info");
+  pokeHeight.classList.add("poke-height", "info");
+  pokeWeight.classList.add("poke-weight", "info");
+
+  pokeName.textContent = `Pokemon Name: ${name}`;
+  pokeHeight.textContent = `Pokemon Height (Feet): ${Math.round(height/3.048)} `;
+  pokeWeight.textContent = `Pokemon weight (Pounds): ${Math.round(weight * 0.441)}`;
+
+  infoCard.appendChild(pokeImg);
+  infoCard.appendChild(pokeName);
+  infoCard.appendChild(pokeHeight);
+  infoCard.appendChild(pokeWeight);
+
+  return infoCard;
+}
+
+
+const getPokeInfo = (name) => {
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then(res => {
+      document.querySelector(".container").prepend(pokeCardInfo(res.data))
+    })
+    .catch(err => console.log(err));
+}
+
+
+const createAbilityCard = () => {
+
+  const abilityCard = document.createElement('div');
+  const abilityCardHeader = document.createElement("h1")
+  const abilityHeader = 'Abilities'
+  abilityCardHeader.textContent = abilityHeader;
+
+  abilityCard.classList.add("ability-card")
+  abilityCardHeader.classList.add("ability-header");
+
+  abilityCard.appendChild(abilityCardHeader);
+
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then(res => {
+      console.log(res.data.abilities)
+      res.data.abilities.forEach(ability => {
+        console.log(ability.ability.name)
+        const abilityTitle = document.createElement("h2")
+
+        abilityTitle.textContent = ability.ability.name;
+        abilityTitle.classList.add("ability")
+        abilityCard.appendChild(abilityTitle);
+      })
     })
     .catch(err => console.log(err));
 
+  document.querySelector('.container').appendChild(abilityCard)
 
+  return abilityCard;
 }
 
-getPokeInfo('squirtle');
-
-
-{/* 
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
-
-*/}
-
-
-
-const pokeCard = pokecard => {
-  const card = document.createElement("div");
-  const pokeImg = document.createElement("img");
-  
 
 
 
 
-}
+  getPokeInfo(pokemonName)
+  createAbilityCard();
